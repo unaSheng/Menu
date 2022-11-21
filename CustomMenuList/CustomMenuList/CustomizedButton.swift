9c94425df30f96194d50eefc49d1b493c9fd4fe9
liwenxiu <li.wenxiu@hellogroup.com>
2022-11-21 11:47:36 +0800

    update ui

--- CustomMenuList/CustomMenuList/CustomizedButton.swift
+++ CustomMenuList/CustomMenuList/CustomizedButton.swift
@@ -17,67 +17,50 @@ class CustomizedButton: UIButton {
     }
     
     func updateUI(spacing: CGFloat = 0, style: Style = .imageTextAtHorizontal) {
+        contentEdgeInsets = .zero
+        imageEdgeInsets = .zero
+        titleEdgeInsets = .zero
+        
         setNeedsLayout()
         layoutIfNeeded()
         
-        let titleSize = calculateTitleSize()
-        let imageSize = calculateImageSize()
-        let originSize = self.bounds.size
-        let width = max(titleSize.width, imageSize.width)
-        let height = titleSize.height + imageSize.height + spacing
-        let contentMarginH = min(0, width - originSize.width)
-        let contentMarginV = max(0, height - originSize.height)
         switch style {
         case .imageTextAtHorizontal:
             self.contentEdgeInsets = UIEdgeInsets(top: 0, left: spacing, bottom: 0, right: 0)
-            self.imageEdgeInsets = UIEdgeInsets(top: 0, left: -spacing * 2, bottom: 0, right: 0)
+            self.imageEdgeInsets = UIEdgeInsets(top: 0, left: -spacing, bottom: 0, right: spacing)
         case .textImageAtHorizontal:
             self.semanticContentAttribute = .forceRightToLeft
             self.contentEdgeInsets = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: spacing)
-            self.imageEdgeInsets = UIEdgeInsets(top: 0, left: spacing * 2, bottom: 0, right: 0)
-        case .imageTextAtVertical:
-            self.contentEdgeInsets = UIEdgeInsets(top: contentMarginV,
-                                                  left: contentMarginH,
-                                                  bottom: 0,
-                                                  right: 0)
-            if titleSize.width > imageSize.width {
-                self.imageEdgeInsets = UIEdgeInsets(top: 2 * (imageSize.height - height),
-                                                    left: 0,
-                                                    bottom: 0,
-                                                    right: -(titleSize.width + imageSize.width))
-            } else {
-                self.imageEdgeInsets = UIEdgeInsets(top:  2 * (imageSize.height - height),
-                                                    left: titleSize.width,
-                                                    bottom: 0,
-                                                    right: 0)
-                self.titleEdgeInsets = UIEdgeInsets(top: 0,
-                                                    left: titleSize.width - width
-                                                    , bottom: 0,
-                                                    right: 0)
-            }
-        case .textImageAtVertical:
-            self.contentEdgeInsets = UIEdgeInsets(top: contentMarginV,
-                                                  left: contentMarginH,
-                                                  bottom: 0,
-                                                  right: 0)
-            if titleSize.width > imageSize.width {
-                self.titleEdgeInsets = UIEdgeInsets(top: 2 * (titleSize.height - height),
-                                                    left: 2 * (titleSize.width - width),
-                                                    bottom: 0,
-                                                    right: 0)
-                self.imageEdgeInsets = UIEdgeInsets(top: 0,
-                                                    left: 0,
-                                                    bottom: 0,
-                                                    right: -(titleSize.width + imageSize.width))
-            } else {
-                self.titleEdgeInsets = UIEdgeInsets(top: 2 * (titleSize.height - height),
-                                                    left: (titleSize.width - width),
-                                                    bottom: 0,
-                                                    right: 0)
-                self.imageEdgeInsets = UIEdgeInsets(top: 0,
-                                                    left: titleSize.width,
-                                                    bottom: 0, right: 0)
-            }
+            self.imageEdgeInsets = UIEdgeInsets(top: 0, left: spacing, bottom: 0, right: -spacing)
+        case .imageTextAtVertical, .textImageAtVertical:
+            let titleSize = calculateTitleSize()
+            let imageSize = calculateImageSize()
+            let originSize = self.bounds.size
+            let width = max(titleSize.width, imageSize.width)
+            let height = titleSize.height + imageSize.height + spacing
+            let contentMarginH = max(0, width - originSize.width)
+            let contentMarginV = max(0, height - originSize.height)
+            let contentVerticalInset = contentMarginV / 2.0
+            let contentHorizontalInset = contentMarginH / 2.0
+            let verticalOffset = titleSize.height - imageSize.height
+            let imageVerticalInset = (imageSize.height + spacing + verticalOffset) / 2.0
+            let imageHorizontalInset = titleSize.width / 2.0
+            let titleVerticalInset = (titleSize.height + spacing - verticalOffset) / 2.0
+            let titleHorizontalInset = imageSize.width / 2.0
+            
+            let multiply: CGFloat = style == .imageTextAtVertical ? -1 : 1
+            self.contentEdgeInsets = UIEdgeInsets(top: contentVerticalInset,
+                                                  left: contentHorizontalInset,
+                                                  bottom: contentVerticalInset,
+                                                  right: contentHorizontalInset)
+            self.imageEdgeInsets = UIEdgeInsets(top: imageVerticalInset * multiply,
+                                                left: imageHorizontalInset,
+                                                bottom: imageVerticalInset * -multiply,
+                                                right: -imageHorizontalInset)
+            self.titleEdgeInsets = UIEdgeInsets(top: titleVerticalInset * -multiply,
+                                                left: -titleHorizontalInset,
+                                                bottom: titleVerticalInset * multiply,
+                                                right: titleHorizontalInset)
         }
     }
     
